6b117c51a836949ba2c491c4b577228ddd29c5ac
Ramon Cunha <ramon.network@hotmail.com>
2020-09-27 17:53:23 -0300

    Creating controller

--- src/main/java/com/alert/collab/controller/EventController.java
+++ src/main/java/com/alert/collab/controller/EventController.java
@@ -1,4 +1,17 @@
 package com.alert.collab.controller;
 
+import com.alert.collab.service.EventService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/event")
 public class EventController {
+
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
 }
