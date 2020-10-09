ed2404a3ba5eb9bbf80ec9976960ce65b7a5b9a4
Ramon Cunha <ramon.network@hotmail.com>
2020-10-08 22:50:14 -0300

    Adding dependencies for jwt auth

--- src/main/java/com/alert/collab/CollabApplication.java
+++ src/main/java/com/alert/collab/CollabApplication.java
@@ -2,8 +2,9 @@ package com.alert.collab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class CollabApplication {
 
 	public static void main(String[] args) {
