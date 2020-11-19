26072d54c202b8a83e6441155a95ab5c9eef0be3
Ramon Cunha <ramon.network@hotmail.com>
2020-11-18 21:39:42 -0300

    Creating doFilterInternal

---
+++ src/main/java/com/alert/collab/security/filter/AuthenticationTokenFilter.java
@@ -0,0 +1,44 @@
+package com.alert.collab.security.filter;
+
+import com.alert.collab.security.service.UserServiceImpl;
+import com.alert.collab.util.JwtTokenUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class AuthenticationTokenFilter extends OncePerRequestFilter {
+    private final String AUTH_HEADER = "Authorization";
+    private final String BEARER_PREFIX = "Bearer ";
+    private final UserServiceImpl userService;
+    private final JwtTokenUtils jwtTokenUtils;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        String token = httpServletRequest.getHeader(AUTH_HEADER);
+        if(token != null && token.startsWith(BEARER_PREFIX)) {
+            token = token.substring(7);
+        }
+        String username = jwtTokenUtils.getUsernameFromToken(token);
+
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userService.loadUserByUsername(username);
+
+            if(jwtTokenUtils.tokenValido(token)) {
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}
---
+++ src/main/java/com/alert/collab/util/JwtTokenUtils.java
@@ -0,0 +1,64 @@
+package com.alert.collab.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.impl.DefaultClaims;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+
+@Component
+public class JwtTokenUtils {
+    public static final String CLAIM_KEY_USERNAME = "sub";
+    public static final String CLAIM_KEY_ROLE = "role";
+    public static final String CLAIM_KEY_CREATED = "created";
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("{jwt.expiration}")
+    private Long expiration;
+
+    public String getUsernameFromToken(String token) {
+        String username;
+        try {
+            Claims claims = getClaimsFromToken(token);
+            username = claims.getSubject();
+        } catch (Exception ex) {
+            username = "";
+        }
+        return username;
+    }
+
+    private Claims getClaimsFromToken(String token) {
+        Claims claims;
+        try {
+            claims = Jwts.parser().setSigningKey(this.secret)
+                    .parseClaimsJws(token).getBody();
+        } catch (Exception ex) {
+            claims = new DefaultClaims();
+        }
+        return claims;
+    }
+
+    public boolean tokenValido(String token) {
+        return !tokenExpirado(token);
+    }
+
+    private boolean tokenExpirado(String token) {
+        Date dataExpiracao = this.getExpirationDateFromToken(token);
+        Date now = new Date();
+        return dataExpiracao.before(now);
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        Date expiration;
+        try {
+            Claims claims = getClaimsFromToken(token);
+            expiration = claims.getExpiration();
+        } catch (Exception ex) {
+            expiration = new Date();
+        }
+        return expiration;
+    }
+}
