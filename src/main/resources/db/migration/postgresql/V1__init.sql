9007d4088dc38b9c522e23792801c86c04654ebb
Ramon Cunha <ramon.network@hotmail.com>
2020-12-01 21:03:49 -0300

    Adjusting column's types of script

--- src/main/resources/db/migration/postgresql/V1__init.sql
+++ src/main/resources/db/migration/postgresql/V1__init.sql
@@ -1,22 +1,22 @@
 CREATE TABLE event (
-    id bigint(20) NOT NULL,
+    id bigint NOT NULL,
     name varchar(255) NOT NULL,
     severity varchar(200) NOT NULL,
     description varchar(255) DEFAULT NULL,
-    latitude DECIMAL(8,6) NOT NULL,
-    longitude DECIMAL(9,6) NOT NULL,
+    latitude NUMERIC(8,6) NOT NULL,
+    longitude NUMERIC(9,6) NOT NULL,
     event_date datetime NOT NULL,
     created_at datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHAR SET=utf8;
+) DEFAULT CHAR SET=utf8;
 
 ALTER TABLE event ADD PRIMARY KEY(id);
-ALTER TABLE event MODIFY id bigint(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE event MODIFY id bigint NOT NULL AUTO_INCREMENT;
 
 CREATE TABLE user (
-    id bigint(20) NOT NULL,
+    id bigint NOT NULL,
     username varchar(50) NOT NULL,
     password varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) DEFAULT CHARSET=utf8;
 
 ALTER TABLE user ADD PRIMARY KEY (id) ;
-ALTER TABLE user MODIFY id bigint(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE user MODIFY id bigint NOT NULL AUTO_INCREMENT;
