7ad1af471741e0f370041d8edaaaaf22eb763d7d
Ramon Cunha <ramon.network@hotmail.com>
2020-12-01 21:05:28 -0300

    Removing DEFAULT CHARSET from SQL script

--- src/main/resources/db/migration/postgresql/V1__init.sql
+++ src/main/resources/db/migration/postgresql/V1__init.sql
@@ -7,7 +7,7 @@ CREATE TABLE event (
     longitude NUMERIC(9,6) NOT NULL,
     event_date datetime NOT NULL,
     created_at datetime NOT NULL
-) DEFAULT CHAR SET=utf8;
+);
 
 ALTER TABLE event ADD PRIMARY KEY(id);
 ALTER TABLE event MODIFY id bigint NOT NULL AUTO_INCREMENT;
@@ -16,7 +16,7 @@ CREATE TABLE user (
     id bigint NOT NULL,
     username varchar(50) NOT NULL,
     password varchar(255) NOT NULL
-) DEFAULT CHARSET=utf8;
+);
 
 ALTER TABLE user ADD PRIMARY KEY (id) ;
 ALTER TABLE user MODIFY id bigint NOT NULL AUTO_INCREMENT;
